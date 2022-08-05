0f5802e5c244b0ec11ae94a49795fa606c4a9839
Edsam2002 <edsamuel1st@gmail.com>
2022-08-05 08:18:47 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-"""A script that stores arguments to a JSON file."""
+"""add item"""
+
+
+import json
 import sys
-import os
-from importlib import import_module as using
+import os.path
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file, load_from_json_file = (
-    using('5-save_to_json_file').save_to_json_file,
-    using('6-load_from_json_file').load_from_json_file
-)
-args_list = []
-file_name = 'add_item.json'
-
-
-def makeScript():
-    if not os.path.exists(file_name):
-        with open(file_name, mode='w', encoding='utf-8') as file:
-            file.write('[]')
-    json_list = load_from_json_file(file_name)
-    if (type(json_list) is list) and all(type(s) is str for s in json_list):
-        args_list.extend(json_list)
-    args_list.extend(sys.argv[1:])
-    save_to_json_file(args_list, file_name)
-
-
-if __name__ == '__main__':
-    makeScript()
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
